4550a54a70875340ec75d4b21250f1c55a3feb4b
RviP9973 <rp031776@gmail.com>
2025-08-19 22:42:45 +0530

    initial commit

---
+++ app/login/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import Link from "next/link";
+import Logo from "@/public/logo.svg";
+import { LoginForm } from "@/components/forms/LoginForm";
+
+export default function Login() {
+  return (
+    <div className="min-h-screen w-screen flex items-center justify-center ">
+      <div className="flex w-full max-w-sm flex-col gap-6 ">
+        <Link href="/" className="flex items-center gap-2 self-center">
+          <Image src={Logo} alt="Logo" className="size-10" />
+          <h1 className="text-2xl font-bold">Dream<span className="text-primary">Jobs</span></h1>
+        </Link>
+        <LoginForm />
+      </div>
+    </div>
+  );
+}
